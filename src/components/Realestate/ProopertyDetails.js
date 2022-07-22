4ae883b4219f1cee66bb2655d47b5f00bd578136
Kinjal84 <kinjalbosamiya621@gmail.com>
2022-07-22 18:45:49 +0530

    Card and Image fixed

--- src/components/Realestate/ProopertyDetails.js
+++ src/components/Realestate/ProopertyDetails.js
@@ -1,5 +1,7 @@
 import React from 'react';
 import bedroom from '../../assets/images/bedroom.jpg';
+import LiveCity from '../../assets/images/LiveCity.jpg';
+import LiveCity2 from '../../assets/images/LiveCity2.jpg';
 import '../../assets/styles/common.scss';
 import '../Realestate/PropertyDetails.scss';
 import { FaBath, FaBed, FaCar } from 'react-icons/fa';
@@ -12,16 +14,25 @@ const Property = () => {
                 title: 'OLEA',
                 place: 'Caulfield North, Victoria',
                 img: bedroom,
+                bedcount: '1-3',
+                bath: '1-3',
+                car: '1-2',
             },
             {
                 title: 'Live City',
                 place: 'Footscray, Victoria',
-                img: bedroom,
+                img: LiveCity,
+                bedcount: '1-3',
+                bath: '1-2',
+                car: '0-2',
             },
             {
                 title: 'Victoria & Albert, Broadbeach',
                 place: 'Broadbeach Queensland',
-                img: bedroom,
+                img: LiveCity2,
+                bedcount: '1-3',
+                bath: '1-3',
+                car: '1-2',
             },
         ],
     };
@@ -34,21 +45,37 @@ const Property = () => {
                         Australia's best investment property deals
                     </h3>
                 </div>
-                <div className='property-info flex'>
+                <div className='card flex justify--content_space-between'>
                     {propertyDetails.information.map((info) => (
-                        <div className='property-img justify--content_space-between'>
+                        <div className='property-img-details '>
                             <img
                                 src={info.img}
                                 alt='bedroom'
                                 className='bedroom-img'></img>
-                            <div>
-                                <p>{info.title}</p>
-                            </div>
-                            <div>
-                                <p>{info.place}</p>
-                            </div>
-                            <div>
-                                <FaBath />
+                            <div className='property-info p--10'>
+                                <div>
+                                    <h4>{info.title}</h4>
+                                </div>
+                                <div>
+                                    <p>{info.place}</p>
+                                </div>
+                                <div className='flex justify--content_space-between'>
+                                    <div className='flex'>
+                                        <div className='p--10'>
+                                            <FaBed />
+                                            {info.bedcount}
+                                        </div>
+                                        <div className='p--10'>
+                                            <FaBath />
+                                            {info.bath}
+                                        </div>
+                                        <div className='p--10'>
+                                            <FaCar />
+                                            {info.car}
+                                        </div>
+                                    </div>
+                                    <div>Apartment</div>
+                                </div>
                             </div>
                         </div>
                     ))}
